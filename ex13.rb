1639f33a2fe0c1da44db6d9b4d19993a6d8511d3
CourtneyS <courtney.sowter@gmail.com>
2014-05-15 10:27:52 +1000

    Exercise 13

---
+++ ex13.rb
@@ -0,0 +1,6 @@
+first, second, third = ARGV
+
+puts "The scipt is called: #{$0}"
+puts "your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
